482ff123b4814dd5be5e03c2a87a60645e342341
Chris Laughlin <bluntm@gmail.com>
2015-09-13 19:19:04 +0100

    adding people count to index

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
+var cheerio = require('cheerio');
+var url = 'http://www.puregym.com/gyms/belfast/whats-happening';
+
+function getNoPeopleInGym(cb) {
+    request(url, function (error, response, html) {
+        var people = 0;
+        if (!error) {
+            var $ = cheerio.load(html);
+            people = $('.people-number').html();
+            console.log(people);
+        }
+        cb(people);
+    });
+}
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    getNoPeopleInGym(function(people) {
+        res.render('index', {title: 'Data Miner', people: people});
+    })                                                         ;
 });
 
 module.exports = router;
--- routes/mineGym.js
+++ routes/mineGym.js
@@ -6,15 +6,7 @@ var cheerio = require('cheerio');
 /* GET users listing. */
 router.get('/', function(req, res, next) {
     var url = 'http://www.puregym.com/gyms/belfast/whats-happening';
-    request(url, function(error, response, html){
-        var people = 0;
-        if(!error){
-            var $ = cheerio.load(html);
-            people = $('.people-number').html();
-            console.log(people);
-        }
-        res.send('people: ' + people);
-    });
+
 });
 
 module.exports = router;
