9d4eeaa9cc62d4b990084456456adbec15f1ad3e
Chris Laughlin <bluntm@gmail.com>
2015-09-13 18:22:00 +0100

    adding basic express application

--- routes/mineGym.js
+++ routes/mineGym.js
@@ -1,21 +1,20 @@
 var express = require('express');
-var fs = require('fs');
+var router = express.Router();
 var request = require('request');
 var cheerio = require('cheerio');
-var app     = express();
 
-app.get('/scrape', function(req, res){
+/* GET users listing. */
+router.get('/', function(req, res, next) {
     var url = 'http://www.puregym.com/gyms/belfast/whats-happening';
     request(url, function(error, response, html){
+        var people = 0;
         if(!error){
             var $ = cheerio.load(html);
-            console.log($('.people-number').html());
+            people = $('.people-number').html();
+            console.log(people);
         }
-    })
+        res.send('people: ' + people);
+    });
 });
 
-app.listen('8081');
-
-console.log('Magic happens on port 8081');
-
-exports = module.exports = app;
+module.exports = router;
